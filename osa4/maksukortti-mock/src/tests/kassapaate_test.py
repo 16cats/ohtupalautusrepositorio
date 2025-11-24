1ac7c36f3bdbc4fd14debe7ded6a2773939fcf14
suski <lequyanh@jyu.fi>
2025-11-24 23:25:34 +0200

    finished osa4 tasks 1-2, added .gitignore

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,20 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_korttia_ladataan_moneen_kertaan(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+        self.kassa.lataa(maksukortti_mock, 20)
+
+        maksukortti_mock.lataa.assert_has_calls([unittest.mock.call(10), unittest.mock.call(20)])
+
+    def test_myydyt_lounaat_kasvaa_oikein(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        assert self.kassa.myytyja_lounaita == 2
